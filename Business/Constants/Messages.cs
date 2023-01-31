6cb333c30806bdd2c6ec965fdad23aa805d2297b
Ögeday Karadal <ogedaykaradal@gmail.com>
2023-01-31 13:50:07 +0300

    Add Constants.Messages

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Car has been added.";
+        public static string CarUpdated = "Car information has been updated.";
+        public static string CarRemoved = "Car has been removed.";
+        public static string CarNotFound = "Car was not found.";
+
+        public static string ColorAdded = "Color has been added.";
+        public static string ColorUpdated = "Color information has been updated.";
+        public static string ColorRemoved = "Color has been removed.";
+        public static string ColorNotFound = "Color was not found.";
+
+        public static string BrandAdded = "Brand has been added.";
+        public static string BrandUpdated = "Brand information has been updated.";
+        public static string BrandRemoved = "Brand has been removed.";
+        public static string BrandNotFound = "Brand was not found.";
+    }
+}
