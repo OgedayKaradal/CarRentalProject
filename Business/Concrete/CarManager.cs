bc9db13ef88fa7fce401f67696ef94cf4fb31290
Ögeday Karadal <ogedaykaradal@gmail.com>
2023-01-31 15:48:31 +0300

    Add Managers

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager:ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public IResult Add(Car car)
+        {
+            _carDal.Add(car);
+            return new SuccessResult(Messages.CarAdded);
+        }
+
+        public IResult Delete(Car car)
+        {
+            _carDal.Delete(car);
+            return new SuccessResult(Messages.CarRemoved);
+        }
+
+        public IDataResult<List<Car>> GetAll()
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll());
+        }
+
+        public IDataResult<Car> GetByCarId(int id)
+        {
+            return new SuccessDataResult<Car>(_carDal.Get(c => c.CarId == id));
+        }
+
+        public IDataResult<List<Car>> GetByName(string carName)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.CarName == carName));
+        }
+
+        public IResult Update(Car car)
+        {
+            _carDal.Update(car);
+            return new SuccessResult(Messages.CarUpdated);
+        }
+    }
+}
---
+++ Business/Concrete/ColorManager.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Abstract;
+using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+using Core.Utilities.Results.Concrete;
+using Business.Constants;
+
+namespace Business.Concrete
+{
+    public class ColorManager:IColorService
+    {
+        IColorDal _colorDal;
+        public ColorManager(IColorDal colorDal)
+        {
+            _colorDal = colorDal;
+        }
+
+        public IResult Add(Color color)
+        {
+            _colorDal.Add(color);
+            return new SuccessResult(Messages.ColorAdded);
+        }
+
+        public IResult Delete(Color color)
+        {
+            _colorDal.Delete(color);
+            return new SuccessResult(Messages.ColorRemoved);
+        }
+
+        public IDataResult<List<Color>> GetAll()
+        {
+            return new SuccessDataResult<List<Color>>(_colorDal.GetAll());
+        }
+
+        public IDataResult<Color> GetById(int id)
+        {
+            return new SuccessDataResult<Color>(_colorDal.Get(c => c.ColorId == id));
+        }
+
+        public IDataResult<List<Color>> GetByName(string colorName)
+        {
+            return new SuccessDataResult<List<Color>>(_colorDal.GetAll(c => c.ColorName == colorName));
+        }
+
+        public IResult Update(Color color)
+        {
+            _colorDal.Update(color);
+            return new SuccessResult(Messages.ColorUpdated);
+        }
+    }
+}
