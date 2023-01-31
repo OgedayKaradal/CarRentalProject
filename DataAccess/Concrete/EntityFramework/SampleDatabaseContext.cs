fcbfd21ca60e9ca98ef493f667055291acc739bf
Ögeday Karadal <ogedaykaradal@gmail.com>
2023-01-31 22:36:42 +0300

    Implement Entity Framework

---
+++ DataAccess/Concrete/EntityFramework/SampleDatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class SampleDatabaseContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=SampleCarDatabase;User Id=postgres;Password=pgadmin;");
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
