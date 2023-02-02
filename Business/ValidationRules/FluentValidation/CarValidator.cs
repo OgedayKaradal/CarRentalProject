7f01445ae361f71ce95fb951b41d8f1bad1dc4e7
Ögeday Karadal <ogedaykaradal@gmail.com>
2023-02-02 18:46:28 +0300

    Add FluentValidation and Validation Rules

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(car => car.CarName).NotEmpty();
+            RuleFor(car => car.BrandId).NotEmpty();
+            RuleFor(car => car.ColorId).NotEmpty();
+            RuleFor(car => car.DailyPrice).NotEmpty();
+            RuleFor(car => car.CarName).MinimumLength(2);
+            RuleFor(car => car.DailyPrice).GreaterThan(0);
+        }
+    }
+}
